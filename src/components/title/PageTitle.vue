b06488db936e63a56ad402f556243182eea19a62
mika1119 <mika1119.work@gmail.com>
2026-05-24 15:28:31 +0900

    create: page title

---
+++ src/components/title/PageTitle.vue
@@ -0,0 +1,50 @@
+<script setup>  
+  defineProps({
+    label: {
+      type: Boolean,
+      default: true
+    },
+    text: {
+      type: String
+    }
+  })
+</script>
+
+<template>
+  <h2 class="page-title">
+    <img src="@/assets/images/decoration.webp" class="page-title__image" alt="ページタイトルの装飾" />
+    <div class="page-title-group">
+      <span v-if="label" class="page-title-group__label">車・バイク</span>
+      <div class="page-title-group__text">{{ text }}</div>
+    </div>
+    <img src="@/assets/images/decoration.webp" class="page-title__image" alt="ページタイトルの装飾" />
+  </h2>
+</template>
+
+<style lang="scss" scoped>
+  .page-title {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1.6rem;
+    &__image {
+      width: clamp(5rem, 10vw, 8rem);
+      height: auto;
+    }
+  }
+
+  .page-title-group {
+    width: auto;
+    height: 100%;
+    display: grid;
+    place-items: center;
+    &__label {
+      font-size: clamp(1.4rem, 2.4vw, 2.2rem);
+    }
+    &__text {
+      font-size: clamp(1.6rem, 3.4vw, 3.8rem);
+    }
+  }
+</style>
