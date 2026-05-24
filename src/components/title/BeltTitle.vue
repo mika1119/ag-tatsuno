d3a7754fee1a7de1613f673ffb3e7203922d315d
mika1119 <mika1119.work@gmail.com>
2026-05-24 15:38:11 +0900

    create: belt title

---
+++ src/components/title/BeltTitle.vue
@@ -0,0 +1,27 @@
+<script setup>  
+  defineProps({
+    text: {
+      type: String
+    }
+  })
+</script>
+
+<template>
+  <div class="belt-title">
+    {{ text }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .belt-title {
+    width: 100%;
+    height: auto;
+    font-size: clamp(1.4rem, 2.4vw, 1.8rem);
+    font-weight: 900;
+    color: $white;
+    background-color: $color-main;
+    display: grid;
+    place-items: center;
+    padding: 1.6rem;
+  }
+</style>
