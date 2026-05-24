e59416ab68bc09b3819c02255c905a2ef8740d4f
mika1119 <mika1119.work@gmail.com>
2026-05-24 13:45:54 +0900

    create: bg section

--- src/components/layout/footer/Footer.vue
+++ src/components/layout/footer/Footer.vue
@@ -1,15 +1,18 @@
 <script setup>
+  import BgSection from '@/components/section/BgSection.vue';
   import FooterContents from '@/components/layout/footer/FooterContents.vue';
   import FooterNav from '@/components/layout/footer/FooterNav.vue';
   import Copyright from '@/components/layout/footer/Copyright.vue';
 </script>
 
 <template>
-  <footer class="footer">
-    <FooterContents />
-    <FooterNav />
-    <Copyright />
-  </footer>
+  <BgSection color="main">
+    <footer class="footer">
+      <FooterContents />
+      <FooterNav />
+      <Copyright />
+    </footer>
+  </BgSection>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/section/BgSection.vue
@@ -0,0 +1,30 @@
+<script setup>
+  defineProps({
+    color: {
+      type: String,
+      default: 'white'
+    }
+  })
+</script>
+
+<template>
+  <div :class="['bg-section', `bg-section_${color}`]">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .bg-section {
+    width: 100%;
+    height: auto;
+    &_white {
+      background-color: $white;
+    }
+    &_main-lightest {
+      background-color: $color-main-lightest;
+    }
+    &_main {
+      background-color: $color-main;
+    }
+  }
+</style>
