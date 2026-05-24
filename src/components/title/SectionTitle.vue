2accbf49e815d2c1a672314cc2da710b111ad66c
mika1119 <mika1119.work@gmail.com>
2026-05-24 15:32:36 +0900

    create: section title

---
+++ src/components/title/SectionTitle.vue
@@ -0,0 +1,27 @@
+<script setup>  
+  defineProps({
+    text: {
+      type: String
+    }
+  })
+</script>
+
+<template>
+  <h2 class="section-title">
+    <div class="section-title__text">{{ text }}</div>
+  </h2>
+</template>
+
+<style lang="scss" scoped>
+  .section-title {
+    width: 100%;
+    height: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1.6rem;
+    &__text {
+      font-size: clamp(1.6rem, 3.4vw, 3.8rem);
+    }
+  }
+</style>
