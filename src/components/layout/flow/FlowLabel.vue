ea97b79991d9ca1303703dda99535e760e7a17e2
mika1119 <mika1119.work@gmail.com>
2026-05-25 09:28:24 +0900

    create: flow label

---
+++ src/components/layout/flow/FlowLabel.vue
@@ -0,0 +1,54 @@
+<script setup>  
+  defineProps({
+    icon: {
+      type: String
+    },
+    num: {
+      type: String
+    }
+  })
+</script>
+
+<template>
+  <div class="flow-label-wrap">
+    <!-- TODO:アイコン画像の受け渡しを修正する -->
+    <img src="@/assets/images/phone.png" class="flow-label-wrap__icon" alt="フローのアイコン">
+    <div class="flow-label">
+      <div class="flow-label__text">STEP</div>
+      <div class="flow-label__num">{{ num }}</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .flow-label-wrap {
+    width: 100%;
+    height: auto;
+    background-color: $color-main-light;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem;
+    &__icon {
+      width: clamp(3.2rem, 4vw, 4rem);
+      height: auto;
+      filter: brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg) brightness(103%) contrast(103%);
+    }
+  }
+
+  .flow-label {
+    font-weight: 900;
+    color: $white;
+    display: flex;
+    justify-content: center;
+    align-items: baseline;
+    gap: 0.2rem;
+    &__text {
+      font-size: clamp(1.4rem, 3vw, 1.6rem);
+    }
+    &__num {
+      font-size: clamp(1.6rem, 4vw, 2rem);
+    }
+  }
+</style>
