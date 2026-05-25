8c29f7ced80a0ddf4add79daf88be4d0b661e5ae
mika1119 <mika1119.work@gmail.com>
2026-05-25 16:05:36 +0900

    create: line title

---
+++ src/components/layout/card/Card.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import LineTitle from '@/components/title/LineTitle.vue';
+</script>
+
+<template>
+  <div class="card">
+    <LineTitle text="車検整備" />
+  </div>
+</template>
---
+++ src/components/title/LineTitle.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+  },
+  position: {
+    type: String,
+    default: 'left'
+  }
+})
+</script>
+
+<template>
+  <div :class="['line-title', `line-title_${position}`]">
+    {{ text }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .line-title {
+    font-size: clamp(1.4rem, 3vw, 2.2rem);
+    font-weight: 600;
+    border-bottom: 1px solid $color-separate;
+    padding: 1rem;
+    @include display(md) {
+      padding: 2rem;
+    }
+    &_center {
+      text-align: center;
+    }
+  }
+</style>
