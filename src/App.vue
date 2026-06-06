2481cc9c2c2bf379ddce07af86d55eff5936095e
mika1119 <mika1119.work@gmail.com>
2026-06-06 16:17:46 +0900

    add: meta tag

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,35 @@
 
   const route = useRoute()
 
-  useHead(() => ({
-    title: `${route.meta.title}｜オートガレージTATSUNO`,
-    meta: [
-      {
-        name: 'description',
-        content: route.meta.description,
-      },
-    ],
-  }))
+  const siteName = 'オートガレージTATSUNO'
+  const siteUrl = 'https://xxxxx.com' // TODO：ドメイン取得後に設定
+
+  useHead(() => {
+    const title = route.meta.title ? `${route.meta.title}｜${siteName}` : siteName
+    const description = route.meta.description
+    const url = `${siteUrl}${route.path}`
+
+    return {
+      title,
+      meta: [
+        { name: 'description', content: description },
+        { property: 'og:title', content: title },
+        { property: 'og:description', content: description },
+        { property: 'og:url', content: url },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: siteName },
+        { property: 'og:locale', content: 'ja_JP' },
+        { property: 'og:image', content: `${siteUrl}/ogp.png` },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: title },
+        { name: 'twitter:description', content: description },
+        { name: 'twitter:image', content: `${siteUrl}/ogp.png` },
+      ],
+      link: [
+        { rel: 'canonical', href: url },
+      ],
+    }
+  })
 
   import Header from '@/components/layout/header/Header.vue';
   import MainSection from '@/components/section/MainSection.vue';
