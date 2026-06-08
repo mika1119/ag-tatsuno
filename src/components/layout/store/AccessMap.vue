521a924f0a539fd1f5565ab28a615b210b354bc1
mika1119 <mika1119.work@gmail.com>
2026-06-08 11:48:15 +0900

    add: map title

--- src/components/layout/store/AccessMap.vue
+++ src/components/layout/store/AccessMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3321.0450359705746!2d130.68896647514728!3d33.65599493855047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f5!3m3!1m2!1s0x3543d5bd37b8d95f%3A0x9b78b41a8039d43c!2z44Kq44O844OI44Ks44Os44O844K4VEFUU1VOTw!5e0!3m2!1sja!2sjp!4v1780286841605!5m2!1sja!2sjp" class="access-map" allow="fullscreen" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3321.0450359705746!2d130.68896647514728!3d33.65599493855047!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f5!3m3!1m2!1s0x3543d5bd37b8d95f%3A0x9b78b41a8039d43c!2z44Kq44O844OI44Ks44Os44O844K4VEFUU1VOTw!5e0!3m2!1sja!2sjp!4v1780286841605!5m2!1sja!2sjp" class="access-map" title="オートガレージTATSUNOの所在地" allow="fullscreen" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </template>
 
 <style lang="scss" scoped>
